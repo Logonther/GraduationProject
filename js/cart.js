2d26c6c3acceb205e0147065025363d062b3e7a9
Logonther <736072727@qq.com>
2019-04-08 17:23:17 +0800

    购物车模态框

--- js/cart.js
+++ js/cart.js
@@ -22,13 +22,13 @@ $(function () {
     function calculate() {
         var totalPrice = 0;
         for (var i = 0; i < $('.goodsList tr.content').length; i++) {
-            $('.goodsList tr.content').eq(i).find('.goodPrice').text($('.goodsList tr.content').eq(i).find('.price').text() *
-                mui($('.goodsList tr.content').eq(i).find('.mui-numbox')).numbox().getValue());
+            $('.goodsList tr.content').eq(i).find('.goodPrice').text(($('.goodsList tr.content').eq(i).find('.price').text() *
+                mui($('.goodsList tr.content').eq(i).find('.mui-numbox')).numbox().getValue()).toFixed(2));
             if ($('.goodsList tr.content input[type=checkbox]').eq(i).prop('checked')) {
-                totalPrice += parseInt($('.goodsList tr.content').eq(i).find('.goodPrice').text());
+                totalPrice += parseFloat($('.goodsList tr.content').eq(i).find('.goodPrice').text());
             }
         }
-        $('#totalPrice').text(totalPrice);
+        $('#totalPrice').text(parseFloat(totalPrice).toFixed(2));
     }
 
     calculate();
@@ -64,5 +64,41 @@ $(function () {
 //改变商品数量后重新进行计算
     $('.mui-btn').on('click',function () {
         calculate();
+    })
+    /*点击确认订单后*/
+    $('.confirm .buy').on('click',function () {
+        var $tr = $('.goodsList tr.content');
+        var n = 0;
+        for (var i = 0; i < $tr.length; i++) {
+            if ($tr.eq(i).find('[type=checkbox]').prop('checked')){
+                $('.confirm-modal tbody').append('<tr class="content">\n' +
+                    '                                    <td>\n' +
+                    '                                        <div class="good">\n' +
+                    '                                            <div class="goodPic">\n' +
+                    '                                                <img src="'+$tr.eq(i).find('img').attr('src')+'">\n' +
+                    '                                            </div>\n' +
+                    '                                            <div class="goodInfo">\n' +
+                    '                                                <div>\n' +
+                    '                                                    <div class="goodName"><a href="javascript:void(0)">'+$tr.eq(i).find('.goodName a').text()+'</a></div>\n' +
+                    '                                                </div>\n' +
+                    '                                            </div>\n' +
+                    '                                        </div>\n' +
+                    '                                    </td>\n' +
+                    '                                    <td class="price">'+$tr.eq(i).find('.price').text()+'</td>\n' +
+                    '                                    <td class="amount">'+mui($tr.eq(i).find('.mui-numbox')).numbox().getValue()+'</td>\n' +
+                    '                                    <td class="goodPrice">'+$tr.eq(i).find('.goodPrice').text()+'</td>\n' +
+                    '                                </tr>');
+            }else{
+                n+=1;
+            }
+        }
+        if(n != $tr.length){
+            console.log(111);
+            $('.confirm-modal').modal('show');
+        }
+    })
+    /*关闭模态框时清除内容*/
+    $('.confirm-modal').on('hide.bs.modal',function () {
+        $('.confirm-modal tbody').empty();
     })
 })
